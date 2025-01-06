42c41629e9f2a4f28e61acc529ff59cf6740b3b6
Ye Seul Kim <yskim940914@gmail.com>
2025-01-06 23:45:42 +0900

    fix: formatting 시 value 제대로 가져오도록 수정

--- formats/linkBlot.js
+++ formats/linkBlot.js
@@ -6,11 +6,26 @@ class LinkBlot extends Inline {
 
   static create(value) {
     const node = super.create();
+    console.log('create', node, value);
     node.setAttribute('href', value.url);
     node.setAttribute('target', value.target);
     node.setAttribute('data-target', value.target);
     return node;
   }
+
+  format(name, value) {
+    console.log('format', name, value);
+    // if (name === 'link' && value) {
+    //   this.domNode.setAttribute('href', value);
+    // } else {
+    //   super.format(name, value);
+    // }
+  }
+
+  static formats(domNode) {
+    return { url: domNode.getAttribute('href'), target: domNode.getAttribute('target') };
+  }
+
 }
 
 Quill.register(LinkBlot);
